7b7a68f18888523751d0fdc309b92da7abafc1d2
Yaremkevych <137343042+Yaremkevych@users.noreply.github.com>
2025-11-15 00:26:25 +0100

    task_4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  } else {
+    {
+      const loginData = {
+        email: email,
+        password: password,
+      };
+      console.log(loginData);
+    }
+  }
+  event.target.reset();
+}
+
+form.addEventListener('submit', handleSubmit);
