2059ff8190219a25797cd41f3d42e0f326737e91
Yaremkevych <137343042+Yaremkevych@users.noreply.github.com>
2025-11-15 00:50:26 +0100

    task_5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  changeBtn: document.querySelector('.change-color'),
+  spanColor: document.querySelector('.color'),
+};
+
+refs.changeBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  refs.spanColor.textContent = randomColor;
+  refs.body.style.backgroundColor = randomColor;
+}
